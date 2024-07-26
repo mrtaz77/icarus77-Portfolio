7f1c679a46645149b273cb33de9fd27b5e68e558
mrtaz77 <mrtazwar23@gmail.com>
2024-07-26 20:07:56 +0600

    socials updated

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        &copy; 2024. Made with ❤️ by{" "}
+        <a href="https://github.com/mrtaz77">icarus77</a>.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/content/socials.js
@@ -0,0 +1,20 @@
+const socials = [
+  {
+    icon: "github.svg",
+    url: "https://github.com/mrtaz77",
+  },
+  {
+    icon: "linkedin.svg",
+    url: "https://www.linkedin.com/in/mostafa-rifat-tazwar/",
+  },
+  {
+    icon: "kaggle.svg",
+    url: "https://www.kaggle.com/icarus77",
+  },
+  {
+    icon: "tryhackme.svg",
+    url: "https://tryhackme.com/p/icarus77",
+  }
+];
+
+export default socials;
